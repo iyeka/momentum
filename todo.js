96f95511c1bd591d90976f898a73e2ced655c9cf
Kaye Lee <dorni@Kayeui-MacBook-Pro.local>
2020-06-21 16:04:53 +0900

    Initial commit

---
+++ todo.js
@@ -0,0 +1,49 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+const toDos = [];
+
+function saveToDos() {
+    
+}
+
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  const span = document.createElement("span");
+  const newId = toDo.length + 1;
+  delBtn.innerText = "❌";
+  span.innerText = text;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  li.id = newId;
+  toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+  toDos.push(toDoObj);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
