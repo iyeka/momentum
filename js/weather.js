747661d8dd93462e04a08afdd593af08c69677ea
iyeka <gayeadang@naver.com>
2022-07-31 20:11:14 +0900

    Momentum by Vanilla JS Complete

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "174df261846ec09e91d921872e9f9eed";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
+}
+
+function onGeoError() {
+    alert("where are you? Can't find your weather.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
---
+++ weekly-review/momentum-week5.md
@@ -0,0 +1,36 @@
+### 이번 주 목표
+
+---
+
+- [ ] _Vanilla JS Momentum 강의 완강_
+- [ ] _Vanilla JS 마지막 과제 제출_
+- [ ] _KOKOA Clone 보완_
+- [ ] _KOKOA Clone 6.36 ~ 6.38 애니메이션 복습_
+- [ ] _Naver AI Tech 4기 지원_
+
+### 이번 주 결과
+
+---
+
+- [✅] _Vanilla JS Momentum 강의 완강_
+- [✅] _Vanilla JS 마지막 과제 제출_
+- [ ] _KOKOA Clone 보완중_
+- [ ] _KOKOA Clone 6.36 ~ 6.38 애니메이션 복습중_
+- [ ] _Naver AI Tech 4기 지원중_
+
+### 😎 기억할 것
+
+---
+
+    - 배열을 문자열로 JSON.stringify
+    - 문자열을 오브젝트로 JSON.parse
+
+## 📌 주간 회고록을 제출해주세요 (필수)
+
+[10주스터디\_Weekly_ProgressReport](https://docs.google.com/spreadsheets/d/1UZNSc5GhAPwFGSGfqMW9qDQjQBzab-5vgoywnQGaPzg/edit?usp=sharing)
+
+## 핵심 3줄 요약 (예시)
+
+- _Naver AI Tech 4기 지원_
+- _KOKOA Clone 6.36 ~ 6.38 애니메이션 복습 및 최종제출 과제 보완_
+- _스스로 js활용할 수 있을까 다음 js week2 과제로 확인해봐야겠다._
